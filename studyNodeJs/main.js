5bd0b6a233e84819e6d9143d789029e664f66233
Edutech <Edutech@EdutechDev>
2017-04-25 16:45:21 +0900

    1. shape module

--- studyNodeJs/main.js
+++ studyNodeJs/main.js
@@ -3,6 +3,9 @@
  */
 //모듈 추출
 var  module = require('./module.js');
-
+var shape = require('./shape.js');
 console.log('abs(-273)=%d',module.abs(-273));
 console.log('circleArea(3)=%d',module.circleArea(3));
+
+console.log('triangeArea(4,3)=%d', shape.triangleArea(4, 3));
+console.log('rectangleArea(5,5)=%d',shape.rectangleArea(5, 5));
---
+++ studyNodeJs/shape.js
@@ -0,0 +1,11 @@
+/**
+ * http://usejsdoc.org/
+ */
+exports.triangleArea = function(width, height){
+	return width * height / 2;
+};
+
+exports.rectangleArea = function(width, height){
+	return width * height;
+};
+
