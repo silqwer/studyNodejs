92d0b95450d006867e7e64e87ccb965b1e760792
Edutech <Edutech@EdutechDev>
2017-04-25 15:39:17 +0900

    2. console.time 연습

---
+++ studyNodeJs/time.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+//시간 측정 시작
+console.time('alpha');
+
+let output = 1;
+
+for(let i=1; i<=10; ++i){
+	output += 1;
+
+}
+
+console.log('Resuilt:',output);
+
+//시간 측정 종료
+console.timeEnd('alpha');
