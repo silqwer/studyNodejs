ea023d785d14796773d8db8a0e304dea38e49c16
Edutech <Edutech@EdutechDev>
2017-04-26 11:30:47 +0900

    1. 이벤트 생성

--- studyNodeJs/processEmit.js
+++ studyNodeJs/processEmit.js
@@ -1,7 +1,7 @@
 process.on('exit', function(code){
 	console.log('안녕히 계세요.');
 });
-console.log('프로그램 실행 중1');
+console.log('프로그램 실행 중1' );
 process.exit();
 console.log('프로그램 실행 중2');
 //이벤트 강제로 발생
